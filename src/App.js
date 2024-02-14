66c125e896fbb1f803561ae83b95d966b478695d
Damaso Garcia Bouza <damasogb@gmail.com>
2024-02-14 10:34:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { MiPrimerEstado } from './components/MiPrimerEstado';
+import Componente1 from './components/Componente1';
+
+function App() {
+
+  const currentYear = new Date().getFullYear().toString();
+
+  console.log(typeof(currentYear));
+  console.log(currentYear);  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>El estado en React - Hook useState</h1>
+        <MiPrimerEstado />
+
+        <br></br>
+        <Componente1 currentYear={currentYear} />
+        
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Componente1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Componente1 = ({ currentYear }) => {
+
+  return (
+    <div>
+      <h4>Año actual: {currentYear}</h4>
+
+    </div>
+  )
+}
+
+export default Componente1;
